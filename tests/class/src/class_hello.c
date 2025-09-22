e6ede936319503160261da6a7b086192e8e6e249
liujiyan <10022504+liujiyan123@user.noreply.gitee.com>
2025-09-22 17:13:36 +0800

    Update class hello test

--- tests/class/src/class_hello.c
+++ tests/class/src/class_hello.c
@@ -2,12 +2,7 @@
 #include <unistd.h>
 
 int main(void) {
-    puts("class_hello: starting");
-    for (int i = 0; i < 5; ++i) {
-        printf("tick %d\n", i);
-        fflush(stdout);
-        usleep(100000);
-    }
-    puts("class_hello: done");
+    puts("Hello world from class_hello.c");
+    // puts("class_hello: done");
     return 0;
 }
